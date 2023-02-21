bcd71ee0a8a366f6ef119cec0afd5acce752d7a1
rob <caplarobko@gmail.com>
2023-02-21 07:29:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="crypto-qr-codes",
-    version="0.1.7",
+    version="0.1.8",
     author="Robert Capla",
     author_email="robert.capla@icloud.com",
     description="A package for generating QR codes for various cryptocurrencies",
